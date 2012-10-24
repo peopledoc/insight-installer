a927d6fff97ab56b540c3a8e78fbd498de634af3
Rémy HUBSCHER <hubscher.remy@gmail.com>
2012-10-24 14:28:15 +0200

    Add a SysInit file for circusd

--- chef/cookbooks/insight/recipes/circus.rb
+++ chef/cookbooks/insight/recipes/circus.rb
@@ -29,6 +29,7 @@ template "#{home}/insight/etc/circus/circus.ini" do
   source "circus.erb"
   action :create
 end
+
 # Install circus
 execute "#{home}/insight/bin/pip install circus Mako MarkupSafe bottle anyjson gevent gevent-socketio gevent_zeromq gevent-websocket greenlet beaker" do
   user username
@@ -36,4 +37,20 @@ execute "#{home}/insight/bin/pip install circus Mako MarkupSafe bottle anyjson g
   environment ({'HOME' => "#{home}"})
   action :run
 end
+
 # Install circushttpd dependencies
+execute "#{home}/insight/bin/pip install Mako MarkupSafe bottle anyjson gevent gevent-socketio gevent_zeromq gevent-websocket greenlet beaker" do
+  user username
+  group username
+  environment ({'HOME' => "#{home}"})
+  action :run
+end
+
+template "/etc/init.d/circusd" do
+  owner "root"
+  group "root"
+  mode "0755"
+  source "circusd.erb"
+  variables({:circus_home => "#{home}/insight"})
+  action :create
+end
